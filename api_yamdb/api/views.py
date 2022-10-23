63d0dbd0efa6d0487560a6fd160d2c8dd81a0dda
MariyaFerulenkova <1940458@gmail.com>
2022-10-23 23:57:22 +0300

    signup_token

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,8 +1,17 @@
-from reviews.models import Title, Category, Genre
-from rest_framework import viewsets
+from django.contrib.auth.tokens import default_token_generator
+from django.core.exceptions import ValidationError
+from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
+from django.utils.translation import gettext_lazy as _
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
-from .serializers import TitleSerializer, CategorySerializer, GenreSerializer,ReviewSerializer, CommentSerializer
+from reviews.models import Title, Category, Genre, Review, User
+from .serializers import TitleSerializer, CategorySerializer, GenreSerializer, SignupSerializer, RecieveTokenSerializer, ReviewSerializer, CommentSerializer
 from .permissions import (AdminModeratorAuthorPermission)
 
 
@@ -41,3 +50,46 @@ class CommentViewSet(viewsets.ModelViewSet):
             Review,
             id=self.kwargs.get('review_id'))
         return review.comments.all()
+
+
+class SignupView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        serializer = SignupSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        user = get_object_or_404(
+            User,
+            username=serializer.validated_data.get('username')
+        )
+        confirmation_code = default_token_generator.make_token(user)
+        send_mail(
+            subject='Confirmation code',
+            message=f'Код подтверждения: {confirmation_code}',
+            from_email=None,
+            recipient_list=[user.email],
+        )
+        return Response(serializer.data)
+
+
+@api_view(['POST'])
+def recieve_token(request):
+    serializer = RecieveTokenSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    confirmation_code = serializer.data.get('confirmation_code')
+    user = get_object_or_404(
+        User,
+        username=serializer.data.get('username')
+    )
+    if not default_token_generator.check_token(user, confirmation_code):
+        raise ValidationError(
+            {'confirmatione_code': _('Invalid confirmation_code')}
+        )
+
+    refresh = RefreshToken.for_user(user)
+
+    return Response(
+        {'access': str(refresh.access_token)}
+    )
