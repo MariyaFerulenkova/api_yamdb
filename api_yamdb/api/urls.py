6b40d5fdadc58b2d0c9d0e7905abd7bf973a0246
Maksim Nerush <nerush-mn@yandex.ru>
2022-10-23 21:55:06 +0200

    Add basenames in router register

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -7,9 +7,21 @@ from .views import TitleViewSet, CategoryViewSet, GenreViewSet
 app_name = 'api'
 
 router_v1 = DefaultRouter()
-router_v1.register('titles', TitleViewSet)
-router_v1.register('categories', CategoryViewSet)
-router_v1.register('genres', GenreViewSet)
+router_v1.register(
+    'titles',
+    TitleViewSet,
+    basename='titles',
+)
+router_v1.register(
+    'categories',
+    CategoryViewSet,
+    basename='categories',
+)
+router_v1.register(
+    'genres',
+    GenreViewSet,
+    basename='genres',
+)
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
